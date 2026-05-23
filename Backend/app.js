2acfbc551382625b10b728cda03abb33d1106b3d
Ritom Paul <rittmann007@gmail.com>
2026-05-23 17:52:55 +0530

    implement cors

--- Backend/app.js
+++ Backend/app.js
@@ -24,6 +24,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
