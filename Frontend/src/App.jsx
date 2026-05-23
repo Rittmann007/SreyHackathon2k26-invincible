63bb6e80ced671a7d5ae99539cc92ff2d2137baa
Ritom Paul <rittmann007@gmail.com>
2026-05-23 18:26:43 +0530

    implement routes

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import {Routes,Route} from "react-router-dom"
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "../features/Auth/Pages/Home";
+import Login from "../features/Auth/Pages/Login";
+import Register from "../features/Auth/Pages/Register";
 
 function App() {
   return (
-    <div>App</div>
+    <>
+    <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/register' element={<Register/>}></Route>
+      </Routes>
+      {/* for error alerts */}
+      <ToastContainer
+        position="bottom-right"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnHover
+        draggable
+        theme="colored"/>
+    </>
   )
 }
 
