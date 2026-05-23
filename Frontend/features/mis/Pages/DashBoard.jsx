07e9437137b57c40a32c1e6d9e603801ba0510a3
Ritom Paul <rittmann007@gmail.com>
2026-05-24 03:16:03 +0530

    initialize dashboard page.Co-authored-by: debaditya d00525242@gmail.com and aashish aashishm3658@gmail.com

---
+++ Frontend/features/mis/Pages/DashBoard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../Auth/Pages/Navbar'
+
+function DashBoard() {
+  return (
+    <>
+    <Navbar/>
+    <div>DashBoard</div>
+    </>
+    
+  )
+}
+
+export default DashBoard
