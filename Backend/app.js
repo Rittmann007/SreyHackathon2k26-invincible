891d2d258ab6490dae5fb9bfae67ba9a66582636
Ritom Paul <rittmann007@gmail.com>
2026-05-24 23:10:18 +0530

    frontend,backend connect

--- Backend/app.js
+++ Backend/app.js
@@ -31,7 +31,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://taskhiveinvincibles.vercel.app/",
   credentials: true
 }))
 
